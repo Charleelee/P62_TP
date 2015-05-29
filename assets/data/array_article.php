7e060990db7cbcb204db1aa016a64e84408bf75f
Charleelee <charleselie.bedard@gmail.com>
2015-05-29 12:35:36 -0400

    Ajout fichier php de base + array_article dans data

---
+++ assets/data/array_article.php
@@ -0,0 +1,18 @@
+<?php
+$article = array(
+		'vg_01' => array(
+				'name' => 'The_Witcher1',
+				'price' => 00,
+				'ref_number' => 'vg_01',
+			),
+		'vg_02' => array(
+				'name' => 'The_Witcher2',
+				'price' => 00,
+				'ref_number' => 'vg_02',
+			),
+		'vg_03' => array(
+				'name' => 'The_Witcher3',
+				'price' => 00,
+				'ref_number' => 'vg_03',
+			),
+		);
