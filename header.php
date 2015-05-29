a4550847601c4683a3fe08061ed62b2c8905d247
Charleelee <charleselie.bedard@gmail.com>
2015-05-29 08:19:00 -0400

    Ajout fichier php de base

---
+++ header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8"/>
+	<meta name="author" content="Jorel Amthor & Charles-Élie Bedard"/>
+	<title>P62_TP Final</title>
+	<link rel="stylesheet" href="assets/css/normalize.css">
+	<link rel="stylesheet" href="assets/css/main.css">
+</head>
