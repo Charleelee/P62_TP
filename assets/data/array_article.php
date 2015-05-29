11dc6add026f81d92a874bdf1dfc105f7b8365dd
Charleelee <charleselie.bedard@gmail.com>
2015-05-29 12:58:42 -0400

    Ajout categorie array_article

--- assets/data/array_article.php
+++ assets/data/array_article.php
@@ -2,16 +2,19 @@
 $article = array(
 		'vg_01' => array(
 				'name' => 'The_Witcher1',
+				'categorie' => 'rpg',
 				'price' => 00,
 				'ref_number' => 'vg_01',
 			),
 		'vg_02' => array(
 				'name' => 'The_Witcher2',
+				'categorie' => 'rpg',
 				'price' => 00,
 				'ref_number' => 'vg_02',
 			),
 		'vg_03' => array(
 				'name' => 'The_Witcher3',
+				'categorie' => 'rpg',
 				'price' => 00,
 				'ref_number' => 'vg_03',
 			),
