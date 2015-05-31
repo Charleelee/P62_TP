dcf70e5541bae4d066cbe580928692124a800746
P85TP <jorel.amthor@gmail.com>
2015-05-31 14:20:24 -0400

    Amélioration du CSS global Rajout d'icône dans le header

--- header.php
+++ header.php
@@ -4,6 +4,9 @@
 			<li><a href="?page=index"><img id="logo" src="assets/img/logo.png"/></a></li>
 			<li><a href="?action=game">Jeux</a></li>
 			<li><a href="?action=cart">Mon panier</a></li>
+			<li><a href="http://www.facebook.com"><img src="assets/img/fb_icon.jpg"></a></li>
+			<li><a href="http://www.twitter.com"><img src="assets/img/twitter-icon.png"></a></li>
+			<li><a href="http://www.instagram.com"><img src="assets/img/insta_icon.png"></a></li>
 		</ul>
 	</nav>
 </header>
--- html_head.php
+++ html_head.php
@@ -5,6 +5,8 @@ echo'
     <meta name="author" content="Jorel Amthor & Charles-Élie Bédard">
     <title>Achats jeux videos</title>
     <link href="assets/css/normalize.css" rel="stylesheet" type="text/css"/>
+    <link href="http://fonts.googleapis.com/css?family=Roboto+Condensed:300" rel="stylesheet"
+    type="text/css"/>
     <link href="assets/css/main.css" rel="stylesheet" type="text/css"/>
 </head>
 ';
