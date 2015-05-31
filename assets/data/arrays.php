b1293dafb867468812f63f578498489856904138
P85TP <jorel.amthor@gmail.com>
2015-05-31 18:22:40 -0400

    Ajout d'élément dans l'array de jeu de la page game.php ( 33 jeu ajouté ) Stylisation de l'affichage des jeux dans la page game.php

--- assets/data/arrays.php
+++ assets/data/arrays.php
@@ -100,4 +100,283 @@ $games_gamesArticles = array(
 			),
 		),
 	),
+	'Baldurs Gate' => array(
+		'rpg' => array(
+			'30$' => array(
+				'vg_03' => array(
+					'assets/img/invisible_small.jpg' => '?game=baldursgate'
+				),
+			),
+		),
+	),
+	'Neverwinter Nights' => array(
+		'rpg' => array(
+			'40$' => array(
+				'vg_04' => array(
+					'assets/img/invisible_small.jpg' => '?game=neverwinter'
+				),
+			),
+		),
+	),
+	'Dungeon Keeper 2' => array(
+		'strategy' => array(
+			'45$' => array(
+				'vg_05' => array(
+					'assets/img/invisible_small.jpg' => '?game=dungeonkeeper2'
+				),
+			),
+		),
+	),
+	'Alpha Centauri' => array(
+		'rpg' => array(
+			'20$' => array(
+				'vg_06' => array(
+					'assets/img/invisible_small.jpg' => '?game=alphacentauri'
+				),
+			),
+		),
+	),
+	'Duke Nukem 3D' => array(
+		'fps' => array(
+			'35$' => array(
+				'vg_07' => array(
+					'assets/img/invisible_small.jpg' => '?game=dukenukem'
+				),
+			),
+		),
+	),
+	'Dragon Shard' => array(
+		'strategy' => array(
+			'20$' => array(
+				'vg_08' => array(
+					'assets/img/invisible_small.jpg' => '?game=dragonshard'
+				),
+			),
+		),
+	),
+	'FarCry 3' => array(
+		'fps' => array(
+			'20$' => array(
+				'vg_09' => array(
+					'assets/img/invisible_small.jpg' => '?game=farcry3'
+				),
+			),
+		),
+	),
+	'Postal 2' => array(
+		'fps' => array(
+			'20$' => array(
+				'vg_10' => array(
+					'assets/img/invisible_small.jpg' => '?game=postal2'
+				),
+			),
+		),
+	),
+	'Unreal Tournament GOTY' => array(
+		'fps' => array(
+			'15$' => array(
+				'vg_11' => array(
+					'assets/img/invisible_small.jpg' => '?game=unreal'
+				),
+			),
+		),
+	),
+	'Ice wind Dale' => array(
+		'rpg' => array(
+			'40$' => array(
+				'vg_12' => array(
+					'assets/img/invisible_small.jpg' => '?game=icewinddale'
+				),
+			),
+		),
+	),
+	'Arcanum' => array(
+		'rpg' => array(
+			'25$' => array(
+				'vg_13' => array(
+					'assets/img/invisible_small.jpg' => '?game=arcanum'
+				),
+			),
+		),
+	),
+	'Commandos' => array(
+		'fps' => array(
+			'5$' => array(
+				'vg_14' => array(
+					'assets/img/invisible_small.jpg' => '?game=commandos'
+				),
+			),
+		),
+	),
+	'S.T.A.L.K.E.R.' => array(
+		'fps' => array(
+			'25$' => array(
+				'vg_15' => array(
+					'assets/img/invisible_small.jpg' => '?game=stalker'
+				),
+			),
+		),
+	),
+	'Bloodrayne' => array(
+		'fps' => array(
+			'20$' => array(
+				'vg_16' => array(
+					'assets/img/invisible_small.jpg' => '?game=bloodrayne'
+				),
+			),
+		),
+	),
+	'Deus EX' => array(
+		'rpg' => array(
+			'60$' => array(
+				'vg_17' => array(
+					'assets/img/invisible_small.jpg' => '?game=deusex'
+				),
+			),
+		),
+	),
+	'Sin Gold' => array(
+		'fps' => array(
+			'2$' => array(
+				'vg_18' => array(
+					'assets/img/invisible_small.jpg' => '?game=singold'
+				),
+			),
+		),
+	),
+	'Colonization' => array(
+		'strategy' => array(
+			'20$' => array(
+				'vg_19' => array(
+					'assets/img/invisible_small.jpg' => '?game=colonization'
+				),
+			),
+		),
+	),
+	'Might and Magic 8' => array(
+		'rpg' => array(
+			'10$' => array(
+				'vg_20' => array(
+					'assets/img/invisible_small.jpg' => '?game=mightandmagic'
+				),
+			),
+		),
+	),
+	'Banished' => array(
+		'strategy' => array(
+			'20$' => array(
+				'vg_21' => array(
+					'assets/img/invisible_small.jpg' => '?game=banished'
+				),
+			),
+		),
+	),
+	'Star wars : Jedi Knight' => array(
+		'fps' => array(
+			'50$' => array(
+				'vg_22' => array(
+					'assets/img/invisible_small.jpg' => '?game=jediknight'
+				),
+			),
+		),
+	),
+	'Gothic 2' => array(
+		'rpg' => array(
+			'10$' => array(
+				'vg_23' => array(
+					'assets/img/invisible_small.jpg' => '?game=gothic2'
+				),
+			),
+		),
+	),
+	'Silver' => array(
+		'rpg' => array(
+			'35$' => array(
+				'vg_24' => array(
+					'assets/img/invisible_small.jpg' => '?game=Silver'
+				),
+			),
+		),
+	),
+	'Silent storm' => array(
+		'strategy' => array(
+			'20$' => array(
+				'vg_25' => array(
+					'assets/img/invisible_small.jpg' => '?game=silentstorm'
+				),
+			),
+		),
+	),
+	'Castles 1 + 2' => array(
+		'strategy' => array(
+			'25$' => array(
+				'vg_26' => array(
+					'assets/img/invisible_small.jpg' => '?game=strategy'
+				),
+			),
+		),
+	),
+	'Rainbow Six' => array(
+		'fps' => array(
+			'40$' => array(
+				'vg_027' => array(
+					'assets/img/invisible_small.jpg' => '?game=rainbowsix'
+				),
+			),
+		),
+	),
+	'Invisible' => array(
+		'fps' => array(
+			'10$' => array(
+				'vg_028' => array(
+					'assets/img/invisible_small.jpg' => '?game=invisble'
+				),
+			),
+		),
+	),
+	'Invisible' => array(
+		'fps' => array(
+			'10$' => array(
+				'vg_029' => array(
+					'assets/img/invisible_small.jpg' => '?game=invisble'
+				),
+			),
+		),
+	),
+	'Invisible' => array(
+		'fps' => array(
+			'10$' => array(
+				'vg_30' => array(
+					'assets/img/invisible_small.jpg' => '?game=invisble'
+				),
+			),
+		),
+	),
+	'Invisible' => array(
+		'fps' => array(
+			'10$' => array(
+				'vg_31' => array(
+					'assets/img/invisible_small.jpg' => '?game=invisble'
+				),
+			),
+		),
+	),
+	'Invisible' => array(
+		'fps' => array(
+			'10$' => array(
+				'vg_32' => array(
+					'assets/img/invisible_small.jpg' => '?game=invisble'
+				),
+			),
+		),
+	),
+	'Invisible' => array(
+		'fps' => array(
+			'10$' => array(
+				'vg_33' => array(
+					'assets/img/invisible_small.jpg' => '?game=invisble'
+				),
+			),
+		),
+	),
 );
