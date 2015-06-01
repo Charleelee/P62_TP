72594be93dc4304e93b43174ddb2de335694bff3
Charleelee <charleselie.bedard@gmail.com>
2015-06-01 07:02:33 -0400

    creation footer

--- footer.php
+++ footer.php
@@ -1,3 +1,60 @@
 <footer>
-	<!-- a venir -->
+	<!-- Background-color rbg(38,38,38) -->
+	<div>
+		<ul>
+			<li><a href="#">REDEEM CODE</a></li>
+			<li><a href="#">RECLAIM YOUR GAME</a></li>
+			<li><a href="#">CONTACT US</a></li>
+			<li><a href="#">CAREER OPPORTUNITIES</a></li>
+			<li><a href="#">SUBMIT YOUR GAME</a></li>
+		</ul>
+	</div>
+	<div>
+		<ul>
+			<li><a href="#">GAMES</a></li>
+			<li><a href="#">ON SALE</a></li>
+			<li><a href="#">BESTSELLERS</a></li>
+			<li><a href="#">Newest releases</a></li>
+			<li><a href="#">Top rated</a></li>
+		</ul>
+		<ul>
+			<li><a href="#">MOVIES</a></li>
+			<li><a href="#">ON SALE</a></li>
+			<li><a href="#">BESTSELLERS</a></li>
+			<li><a href="#">Newest releases</a></li>
+			<li><a href="#">Top rated</a></li>
+			<li><a href="#">Submit your movie</a></li>
+		</ul>
+		<ul>
+			<li><a href="#">SUPPORT</a></li>
+			<li><a href="#">Website FAQ</a></li>
+			<li><a href="#">Games support</a></li>
+			<li><a href="#">Policies</a></li>
+			<li><a href="#">Contact us</a></li>
+		</ul>
+		<ul>
+			<li><a href="#">MY ACCOUNT</a></li>
+			<li><a href="#">My library</a></li>
+			<li><a href="#">Orders & Settings</a></li>
+		</ul>
+		<ul>
+			<li><a href="#">COMMUNITY</a></li>
+			<li><a href="#">Forums</a></li>
+			<li><a href="#">Community Wishlist</a></li>
+		</ul>
+		<ul>
+			<li><a href="#"><img src="" alt="logo"></a></li>
+			<li><a href="#"><img src="" alt="logo"></a></li>
+			<li><a href="#"><img src="" alt="logo"></a></li>
+			<li><a href="#"><img src="" alt="logo"></a></li>
+			<li><a href="#"><img src="" alt="logo"></a></li>
+			<li><a href="#"><img src="" alt="logo"></a></li>
+			<li><a href="#"><img src="" alt="logo"></a></li>
+			<li><a href="#"><img src="" alt="logo"></a></li>
+		</ul>
+	</div>
+	<div>
+		<!-- Background-color rbg(89,89,89) -->
+		<p>Legal</p>
+	</div>
 </footer>
