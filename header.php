8bd0bdc1c7953b2658ad9e10a0a30bbc027fdd18
Charleelee <charleselie.bedard@gmail.com>
2015-06-01 09:30:23 -0400

    modif header icons

--- header.php
+++ header.php
@@ -14,12 +14,9 @@ $nb_item = 1;
 			fa-2x"></i>
 			<span id="cart_items"><?php echo $nb_item ?></span></li>
 			</div>
-			<li id="fb"><a href="http://www.facebook.com"><i class="fa fa-facebook-official
-			fa-2x"></i></a></li>
-			<li id="twitter"><a href="http://www.twitter.com"><i class="fa fa-twitter
-			fa-2x"></i></a></li>
-			<li id="insta" ><a href="http://www.instagram.com"><i class="fa fa-instagram
-			fa-2x"></i></a></li>
+			<li id="fb"><a href="http://www.facebook.com"><img src="assets/img/social/social21.png" alt="logo" width="35px"></a></li>
+			<li id="twitter"><a href="http://www.twitter.com"><img src="assets/img/social/social38.png" alt="logo" width="35px"></a></li>
+			<li id="insta" ><a href="http://www.instagram.com"><img src="assets/img/social/social77.png" alt="logo" width="35px"></a></li>
 		</ul>
 	</nav>
 </header>
