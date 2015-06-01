2b71e304e471c9cdc2b09bbe8a4e93f027a7dae4
Charleelee <charleselie.bedard@gmail.com>
2015-06-01 11:46:17 -0400

    suppression game_info.php, modif game_details.php

--- game_details.php
+++ game_details.php
@@ -1,12 +1,26 @@
-<!DOCTYPE html>
-<html>
-<head lang="en">
-    <meta charset="UTF-8">
-    <title></title>
-</head>
-<body>
 <?php
-echo'Redirection effectuée';
+#Init 
+include('controller.php');
+include('assets/data/arrays.php');
+?>
+<html>
+<?php include ('html_head.php'); ?>
+<body>
+<?php include ('header.php'); ?>
+	<div id="content_wrapper">
+		<article>
+			<h1></h1>
+			<img src="" alt="">
+			<h3></h3>
+			<p></p>
+		</article>
+		<aside>
+			<span></span>
+			<button>ADD TO CART</button>
+		</aside>
+	</div>
+<?php
+include('footer.php');
 ?>
 </body>
 </html>
