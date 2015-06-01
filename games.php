b8720dfedd3564eed02e0bbb549e74b4c95bc591
Charleelee <charleselie.bedard@gmail.com>
2015-05-31 20:16:00 -0400

    refonte de la page game.php en version template, suppression des fonctions

--- games.php
+++ games.php
@@ -1,16 +1,22 @@
 <?php include('controller.php');
 include('assets/data/arrays.php');
-include('assets/data/fonctions.php');
 include ('html_head.php');
 include ('header.php');
 
-
-//Boucle de recupération de données stocké dans l'array qui va éxecuter une fonction
-//En prennant les clé du tableau comme paramètre
-echo "<div id=content_wrapper>";
-foreach ($games_gamesArticles as $key => $v) {
-	generate_gameArticle($v['title'],$v['genre'],$v['price'],$key,$v['img'],$v['url']);
-}
-echo "</div>";
-
+//Boucle de recupération de données stocké dans l'array qui va generer des articles (template)
+//En prennant les clé / value du tableau comme paramètre
+?>
+<div id="content_wrapper">
+<?php foreach ($games_gamesArticles as $key => $v): ?>
+	<a class="<?php echo $v['genre'] ?>" href="<?php echo $v['url'] ?>">
+		<article class="game_product" id="<?php echo $key ?>">
+			<img class="product_img" src="<?php echo $v['img'] ?>" alt='image'>
+			<h3><?php echo $v['title'] ?></h3>
+			<span class="product_price"><?php echo $v['price'] ?></span>
+		</article>
+	</a>
+<?php endforeach ?>
+</div>
+<?php
 include('footer.php');
+?>
