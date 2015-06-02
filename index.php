b88c1811e1358d6225d9d784d336ca458fdf8fbe
Charleelee <charleselie.bedard@gmail.com>
2015-06-02 11:02:53 -0400

    modif index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 #init
-include('assets/data/arrays.php');
+include('assets/data/database_link.php');
 //Boucle de recupération de données stocké dans l'array qui va generer des articles de l'index (template)
 //En prennant les clé / value du tableau comme paramètres
 ?>
@@ -9,13 +9,16 @@ include('assets/data/arrays.php');
 <body>
 <?php include ('header.php'); ?>
     <div id="game_wrapper">
-        <?php foreach ($index_gamesArticles as $key => $v): ?>
-            <a class="<?php echo $v['genre'],' ',$v['size'] ?>" href="<?php echo $v['url'] ?>">
-                <article class="product" id="<?php echo $key ?>">
+        <?php $games = getGames();
+            foreach ($games as $key => $v): ?>
+            <?php if (!empty($v['img_size'])): ?>  
+            <a class="<?php echo $v['genre'],' ',$v['img_size'] ?>" href="game_details.php?url=<?php echo $v['url'] ?>">
+                <article class="product" id="<?php echo $v['id'] ?>">
                     <img class="product_img" src="<?php echo $v['img'] ?>" alt="image">
                     <h3><?php echo $v['title'] ?></h3>
                 </article>
             </a>
+            <?php endif ?>
         <?php endforeach ?>
     </div>
 <?php
