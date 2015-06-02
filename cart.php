cb4b57a3df6828b353d9c340d9fccb28ac9436f2
Charleelee <charleselie.bedard@gmail.com>
2015-06-02 11:43:53 -0400

    push

--- cart.php
+++ cart.php
@@ -1,7 +1,5 @@
 <?php
 #init 
-include('controller.php');
-include('assets/data/arrays.php');
 include('assets/data/database_link.php');
 //Boucle de recupération de données stocké dans l'array qui va generer des articles de l'index (template)
 //En prennant les clé / value du tableau comme paramètre
