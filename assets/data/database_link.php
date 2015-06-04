ed73ce9dd6ecdb7708627653917eb6fe2b9e6c87
Charleelee <charleselie.bedard@gmail.com>
2015-06-03 23:10:11 -0400

    ajout nouvelle fonction getGamesByRand pour page details

--- assets/data/database_link.php
+++ assets/data/database_link.php
@@ -18,6 +18,24 @@ function getGames(){
 
 }
 
+function getGamesByRand(){
+	$connexion = new mysqli('localhost','root','','game_database');
+	if ($connexion->connect_errno) {
+		echo 'Erreur de connexion';
+	}
+	$requete = 'SELECT * FROM games ORDER BY RAND()';
+	$res = $connexion->query($requete);
+	$games = array();
+	if ($res && ($res->num_rows > 0)) {
+		while ($game = $res->fetch_assoc()) {
+			array_push($games, $game);
+		}
+	}
+	 return $games;
+
+
+}
+
 function getGamesByGenre($genre){
 	$connexion = new mysqli('localhost','root','','game_database');
 	if ($connexion->connect_errno) {
