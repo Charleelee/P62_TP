96eabc6fbc258a889eb9f67b93f111e7a5443965
P85TP <jorel.amthor@gmail.com>
2015-06-04 03:21:08 -0400

    Ajout du Readme

--- _loginform.php
+++ _loginform.php
@@ -2,34 +2,36 @@
 
 $authorized=array(
     'Jorel' =>'jorel12',
-    'admin' => 'admin',
-    'user' => 'user'
+    'user' => 'user',
+    'admin' => 'admin'
 );
 
 $user_is_trying_to_connect = isset($_POST['connect']);
 $user_is_connected = isset($_SESSION['login-status']);
 $user_exist = isset($_SESSION['user-info']);
+
+
 $user_want_to_disconnect = isset($_POST['disconnect']);
 
 if($user_is_trying_to_connect){
     foreach($authorized as $key => $value){
         if(($_POST['username']==$key)&&($_POST['password']==$value)){
+            $_SESSION['login']=$_POST['username'];
             $_SESSION['login-status']='logged';
             redirect($_SERVER['PHP_SELF']);
+            break;
         }else{
             unset($_SESSION['login-status']);
             redirect('inscription.php?error=07');
         }
     }
-//    $_SESSION['login-status']='logged';
-//    redirect($_SERVER['PHP_SELF']);
 }
     ?>
     <div id="login_logout_form">
         <?php if ($user_is_connected) { ?>
-            <span><?php foreach($_SESSION['user-info'] as $key => $value){
-                    echo $key;
-                } ; ?></span>
+            <span><?php
+                $user_name= $_SESSION['login'];
+                echo $user_name ; ?></span>
             <form method="post">
                 <input type="submit" name="disconnect" id="se_deconnecter" value="Déconnexion"/>
             </form>
@@ -42,8 +44,8 @@ if($user_is_trying_to_connect){
             ?>
         <?php } else { ?>
             <form method="post" id="">
-                <input type="text" name="username" id="username" value="<?php echo isset($_POST['username']) ? $_POST['username'] : ''; ?>"/><br>
-                <input type="password" name="password" id="password" value="<?php echo isset($_POST['password']) ? $_POST['password'] : ''; ?>"/><br>
+                <input type="text" name="username" id="username"/><br>
+                <input type="password" name="password" id="password"/>
                 <input type="submit" name="connect" id="connectsubmit" value="Connexion"/>
             </form>
         <?php } ?>
