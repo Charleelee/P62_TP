e8aed460f7405160f087bcc71082c9ce739d9884
Charleelee <charleselie.bedard@gmail.com>
2015-06-03 22:20:45 -0400

    css final cart.php + tweaks, ajout a cart.php + fix

--- cart.php
+++ cart.php
@@ -46,6 +46,11 @@ if(!isset($_SESSION['login-status'])){
 
 				if ($_SESSION['cart'][$_GET['game']] == 0) unset($_SESSION['cart'][$_GET['game']]);
 			}
+		case 'checkout':
+			if (isset($_SESSION['cart'])) {
+				unset($_SESSION['cart']);
+				header('location: index.php');
+			}
 	}
 }
 
@@ -55,6 +60,8 @@ if(!isset($_SESSION['login-status'])){
     $subTotal = 0;
     if (sizeof($_SESSION['cart']) > 0): ?>
     <section class="cart-page-section">
+    <a id="promo-link" href="games.php"></a>
+    <img class="promo" src="assets/img/cart_promo.jpg" alt="img">
     <table>
  	
     	<?php foreach($_SESSION['cart'] as $game=>$qty) { 
@@ -62,13 +69,16 @@ if(!isset($_SESSION['login-status'])){
     		$gameData = getOneGame($game);
     		$subTotal += $gameData['price'] * $qty;
     	?>
-    	<tr><td><?php echo $gameData['title'] ?> ( <?php echo $gameData['price'] ?> $) x <?php echo $qty?></td><td> &nbsp; <a href="cart.php?action=remove&game=<?php echo $game ?>">Remove</a></td></tr>
+    	<tr><td><?php echo $gameData['title'] ?> ( <?php echo $gameData['price'] ?> $) x <?php echo $qty?></td><td class="remove-button"> &nbsp; <a href="cart.php?action=remove&game=<?php echo $game ?>">Remove</a></td></tr>
 		<?php } ?>
-    
+    	<tr><td>Subtotal: <?php echo $subTotal.'$' ?></td><td><a href="cart.php?action=checkout">CHECKOUT NOW</a></td></tr>
     </table>
-	<p>Subtotal: <?php echo $subTotal.'$' ?></p>
+    <p>We never save your payment details and the order is protected by secure connection. Your order is also covered by our 30 Day Money Back Guarantee. This purchase will be subject to consumer withdrawal rights, which will end once the digital content is downloaded. </p>
 	<?php else: ?>
-		<p>Le panier est vide</p>
+		<section class="cart-page-section">
+			<img class="promo" src="assets/img/cart_promo.jpg" alt="img">
+			<p>Le panier est vide</p>
+		</section>
 	<?php endif ?>
 	</section>
 </div>
